f3f91841512287c187adc7afad2726f115234c2c
Yumna-07 <11241090@student.itk.ac.id>
2025-05-28 22:40:11 +0800

    oop

---
+++ src/main/java/org/Login/Login.java
@@ -0,0 +1,10 @@
+public class Login {
+    private int id_login;
+    private String Pengguna;
+    private String Password;
+
+    public String getPassword() { return null; }
+    public void getId_login() {}
+    public void getPengguna() {}
+    public boolean valid_login(int id_login, String Password) { return false; }
+}

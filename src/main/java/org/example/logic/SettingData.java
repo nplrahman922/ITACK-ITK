23f18079c35d1b0eb30ca9b63a6b2b84d7a37d96
Sansc0lD <148742241+Sansc0lD@users.noreply.github.com>
2025-06-20 09:53:17 +0800

    Update SettingData.java
    komen tipis tipis

--- src/main/java/org/example/logic/SettingData.java
+++ src/main/java/org/example/logic/SettingData.java
@@ -1,13 +1,13 @@
 package org.example.logic;
 
-
 public interface SettingData {
 
-   
+    //Membuat tabel untuk menyimpan data objek jika belum ada.
     void buat_tabel();
 
+    //Refresh Data Tabel
     void updt_tabel();
 
-    
+    // Menyimpan data objek ke dalam database.
     void Save_data();
 }

01a159a88bedd41449aafe950c7a1f403669e5aa
Sansc0lD <148742241+Sansc0lD@users.noreply.github.com>
2025-06-20 10:04:43 +0800

    Update Main.java
    Update Komen

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,22 +2,16 @@ package org.example;
 
 // Import kelas-kelas yang diperlukan
 import javax.swing.SwingUtilities;
-import org.example.GUI.loginInterface; // Import kelas antarmuka login Anda
+import org.example.GUI.loginInterface;
 import org.example.logic.Login;
 
-/**
- * Kelas Main utama untuk menjalankan seluruh aplikasi ITACK GUI.
- * Ini adalah titik awal (entry point) dari aplikasi.
- */
 public class Main {
     
     public static void main(String[] args) {
-        // semua kode GUI
-        // dijalankan pada 'Event Dispatch Thread',
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                //memuat database
+                //Memuat database
                 Login.initDatabase();
 
                 loginInterface loginFrame = new loginInterface("ITACK - Login");
